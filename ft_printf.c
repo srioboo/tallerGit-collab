5cae46da295edc40e01d9f8e67129683c24fda9d
Jose Antonio Gonzalez Luque <jgonzale@c2r3s3.42malaga.com>
2025-03-04 17:34:45 +0100

    subida_printf_minishell

---
+++ ft_printf.c
@@ -0,0 +1,4 @@
+int ft_printf (char c)
+{
+printf ("hola universo");
+}
--- minishell.c
+++ minishell.c
@@ -6,3 +6,12 @@ int	main(void)
 
 	return (0);
 }
+
+#include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!(s) || fd < 0)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
